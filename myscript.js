1feb303e527a1e43a3452c8a26393c0ece89d02d
rohitaj002 <happyajay002@g,ail.com>
2020-09-28 16:21:53 +0530

    commit on 'master'

---
+++ myscript.js
@@ -0,0 +1,55 @@
+// var textWrapper = document.querySelector('.ml11 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml11 .line',
+//     scaleY: [0,1],
+//     opacity: [0.5,1],
+//     easing: "easeOutExpo",
+//     duration: 700
+//   })
+//   .add({
+//     targets: '.ml11 .line',
+//     translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+//     easing: "easeOutExpo",
+//     duration: 700,
+//     delay: 100
+//   }).add({
+//     targets: '.ml11 .letter',
+//     opacity: [0,1],
+//     easing: "easeOutExpo",
+//     duration: 600,
+//     offset: '-=775',
+//     delay: (el, i) => 34 * (i+1)
+//   }).add({
+//     targets: '.ml11',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
+
+
+
+ 
+
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
